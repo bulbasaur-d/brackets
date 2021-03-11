ad2330840f547ac90f5689b9caa1c9495e78f20f
Dmytro Motuzko <motuzko.d15@gmail.com>
2021-03-11 15:23:29 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let br = bracketsConfig.map(function(arr) {
+        let conStr = arr.join('');
+        return conStr;
+    });
+
+    for (let i = 0; i < br.length; i++) {
+        if (str.includes(br[i])) {
+            str = str.replace(br[i], '');
+            i = -1;
+        }
+
+    }
+    return !str;
+};
